508a07ac03b33cfa09fb9ffd0cade77c73959050
Meri11Ra1 <r.t.00293794@gmail.com>
2025-09-25 22:20:28 +0900

    hiltのパッケージをインポート

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -103,4 +105,8 @@ dependencies {
 
     // serializer
     implementation(libs.kotlinx.serialization.json)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
 }
