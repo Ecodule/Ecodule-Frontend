ce1ea70a7a93ce22604b034fab80eee4ee34f7b2
N-774 <20081207ts@gmail.com>
2025-10-02 23:08:55 +0900

    空白入れただけ。コードに変更なし

--- app/src/main/java/com/example/ecodule/ui/widget/MyWidgetReceiver.kt
+++ app/src/main/java/com/example/ecodule/ui/widget/MyWidgetReceiver.kt
@@ -2,6 +2,7 @@ package com.example.ecodule.ui.widget
 
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
 class MyWidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = AppWidget()
 }
